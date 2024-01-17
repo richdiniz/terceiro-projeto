38c78b2ea6aa4143e87626915b16781e086eb997
casaD <luisdiniz2104@gmail.com>
2024-01-17 20:01:56 -0300

    ñ criei um arquivo em branco e fiz comentarios no codigo

--- af.js
+++ af.js
@@ -1,11 +1,13 @@
 var am=document.getElementById('am')
 var ap=document.getElementById('ap')
 function p() {
+    //troca de valores...
     var m=Number(am.value)
     var p=Number(ap.value)
+    //detrminando alturas...
     var afh=(m+p+12.5)/2
     var afm=(m+p-12.5)/2
-
+    //respostas:
     res.innerHTML+='<p>seu filho terá '+afh+'</p>'
     res.innerHTML+='<p>sua filha terá '+afm+'</p>'
 }
